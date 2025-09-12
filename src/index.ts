95146b139bad8c374aeb08ddeb7f71bae9efa88f
ZxStim <zxstim@gmail.com>
2025-09-12 19:58:13 +0700

    feat: exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+// Function exports
 export { Slpx } from './slpx';
 export * from "./constants";
-export * from "./abis";
+export * from "./slpx-testnet";
+export * from "./types";
+export * from "./abis/manta-slpx-v1-abi";
+export * from "./abis/soneium-slpx-v1-abi";
+export * from "./abis/slpx-v2-abi";
+export * from "./abis/testnet-slpx-v2-abi";
